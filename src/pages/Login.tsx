4918243839b489f87a857f6f2d6be25e59197c0b
Kaua Paixao <kauantnetsoares@gmail.com>
2023-06-28 17:23:26 -0300

    Trabalhando na rota de Login - Lucas

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,30 @@ import '../App.css';
 const Login = () => {
     return (
         <div>
-            <h1 className='h1Login'>PÁGINA DE LOGIN</h1>
+        <body className='bodi'>
+          <form className='log1'>
+        <h2>Login</h2>
+        <div className='box-user'>
+            <input className='usuario1' type="text"></input>
+            <label>Usuário ou Email:</label>
+        </div>
+        <div className='box-user'>
+            <input className='usus1' type="password"></input>
+            <label>Senha:</label>
+        </div>
+        <div>
+            <a className='for1'> Não Possui cadastro?</a>
+        </div>
+        <a href="#" className="butao1">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Entrar
+        </a>
+        
+    </form>
+    </body>
         </div>
     )
 }
