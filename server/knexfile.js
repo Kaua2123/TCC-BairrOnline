a40c82776b85c39cbddae741c5f4c098db1b00ff
Kaua2123 <kauantnetsoares@gmail.com>
2023-07-03 19:46:00 -0300

    Requisição POST funcionando com biblioteca axios - Kauã

--- server/knexfile.js
+++ server/knexfile.js
@@ -3,7 +3,7 @@ module.exports= {
             client: 'mysql',
             connection: {
               host : '127.0.0.1',
-              port : 3001,
+              port : 3306,
               user : 'root',
               password : '',
               database : 'bairronline'
--- server/routes.js
+++ server/routes.js
@@ -19,3 +19,20 @@ routes.post("/teste", controllersUsu.testeCriarUsuario);
  
 
 module.exports = routes;
+
+const axios = require('axios');
+
+const dados = {
+    usu_cod: '4',
+    usu_nome: 'testePOST',
+    usu_email: 'testePOST@gmail.com',
+    usu_senha: 'post123',
+    usu_tel: '2324-9233'
+
+};
+
+axios.post('http://localhost:3344/teste', dados).then((response) => {
+    console.log('REGISTRO ENVIADO ');
+}).catch((error) => {
+    console.error(error);
+});
