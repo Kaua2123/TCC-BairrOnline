2a993509fea9e0a2a45dd274cfa024a3b917016b
Kaua2123 <kauantnetsoares@gmail.com>
2023-07-11 18:16:07 -0300

    Adicionando uma seção mais informativa na home e estilização - Kauã

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -91,8 +91,8 @@ const Home = () => {
  </article>
  <article className="artiOrg">
 <div className='bordinha'>
- <h1 className="h1Org">Instituições Responsáveis</h1>
- <h2 className="subtituloOrg"> Veja aqui as instituições/órgãos públicos que mais resolveram problemas</h2>
+ <h1 className="h1Org">Instituições/Órgãos Públicos</h1>
+ 
 </div>
 <input className="inpOrg" type="search" placeholder='Pesquise por instituições/orgãos publicos aqui'></input>
 <div className='rolagemInst'> <h1>@VRsaneamento</h1>  <h1>@VRsaneamento</h1><h1>@VRsaneamento</h1><h1>@VRsaneamento</h1><h1>@VRsaneamento</h1><h1>@VRsaneamento</h1><h1>@VRsaneamento</h1><h1>@VRsaneamento</h1>  <h1>@VRsaneamento</h1><h1>@VRsaneamento</h1><h1>@VRsaneamento</h1><h1>@VRsaneamento</h1><h1>@VRsaneamento</h1></div> 
@@ -101,13 +101,61 @@ const Home = () => {
 
 
  </div>
-</section>
+</section>  
+     <div className='containerSobre'>
 <div className='sobreNos'>
+
+  <h2 className='tituloSobre'>O que é o Bairronline?</h2>
   
-  <h1 className='tituloSobre'>O que o BairrOnline tem a oferecer?</h1>
+  <div className='textoSobre'>
 
+  BairrOnline é uma aplicação com o foco de facilitar a realização das denúncias referentes a 
+  estrutura de bairros de Volta Redonda, e relatar os mesmos para as instituições relacionadas, dando visibilidade e
+  por conseguinte uma maior chance de resolução dos problemas relatados. Em outras palavras, um portal de denúncias.
+
+  </div>
+  
+  <h2 className='tituloSobre'>Que funções o BairrOnline tem a oferecer?</h2>
+  <div className='textoSobre'>
+
+  O usuário, como denunciante, poderá efetuar as denúncias e visualizar as denúncias de outros usuários. Estas, serão 
+  encaminhadas para as instituições, que decidirão se irão assumir a denúncia para resolver, ou não. Caso assumam alguma 
+  denúncia, esta será exibida para o usuário, que poderá acompanhar a resolução.
+
+  </div>
+
+  <h2 className='tituloSobre'>Por que escolher o BairrOnline?</h2>
+  <div className='textoSobre'>
+
+  Nossa proposta com o BairrOnline, é fazer com que as denúncias tenham visibilidade e sejam de realmente notadas pelas instituições.
+  Faremos o nosso melhor para que de fato, seja uma aplicação eficiente e que satisfaça às expectativas dos usuários da mesma.
+  
+  </div>
+
+  <h2 className='tituloSobre'>Quem somos</h2>
+  <div className='textoSobre'>
+  {/*se botar em quadradinhos com algum texto fica mais legal, tipo os quadradinhos da decolar*/}
+  Gabriel
+  <br />
+  Kauã
+  <br />
+  Lucas
+  <br />
+  Nattan
+  <br />
+  Rayan
+  <br />
+  Thales
+  <br />
+  
+
+  </div>
+
+
+
+  
  </div>
-
+     </div>
     </body>
         
         <footer className="container">
