040a997ddcdcdef9497364e457cd3465a8b2b16d
Kaua2123 <kauantnetsoares@gmail.com>
2023-07-26 19:46:42 -0300

    imagens q vao ser usadas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -91,7 +91,7 @@ const Home = () => {
 
 
                 {/* ------------SEÇÃO DENUNCIAS E INSTITUIÇOES AQUI------------*/}
-              <Box  w="100em" bg='white' h='120em'  borderRadius='4px'>
+              <Box  w="100%" bg='white' h='120em'  borderRadius='4px'>
                   <Flex p='30px'>
                      <Text color='#338BB0' textShadow='0px 2.6px 2px rgb(172, 172, 172);' fontSize='40px' fontFamily='BreeSerif-Regular' fontWeight='extrabold' >Denúncias em alta</Text>
                   </Flex>
