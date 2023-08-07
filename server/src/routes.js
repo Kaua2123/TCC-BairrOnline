f68e29f7fc364fc948d435e0e5dafa6c1ab077a5
Kaua2123 <kauantnetsoares@gmail.com>
2023-08-07 18:43:12 -0300

    alterations

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,15 +2,15 @@ const express = require('express');
 
 
 
-const controllersAcomp = require('./src/controllers/acompanhamento/index');
-const controllersAdm = require('./src/controllers/administrador/index');
-const controllersAva = require('./src/controllers/avaliaçao/index');
-const controllersBairro = require('./src/controllers/bairro/index');
-const controllersComen = require('./src/controllers/comentarios/index');
-const controllersDenunciante = require('./src/controllers/denunciante/index');
-const controllersDenuncias = require('./src/controllers/denuncias/index');
-const controllersOrgInst = require('./src/controllers/orgaoInstituiçao/index');
-const controllersUsu = require('./src/controllers/usuario/index');
+const controllersAcomp = require('../src/controllers/acompanhamento/index');
+const controllersAdm = require('../src/controllers/administrador/index');
+const controllersAva = require('../src/controllers/avaliaçao/index');
+const controllersBairro = require('../src/controllers/bairro/index');
+const controllersComen = require('../src/controllers/comentarios/index');
+const controllersDenunciante = require('../src/controllers/denunciante/index');
+const controllersDenuncias = require('../src/controllers/denuncias/index');
+const controllersOrgInst = require('../src/controllers/orgaoInstituiçao/index');
+const controllersUsu = require('../src/controllers/usuario/index');
 const routes = express.Router();
 
 
