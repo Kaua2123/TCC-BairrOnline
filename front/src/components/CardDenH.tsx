2775c9848f5e524e77f644b64d1a6e8bcf1e101d
Kaua2123 <kauantnetsoares@gmail.com>
2023-08-11 21:11:35 -0300

    modal do chakra pra ampliar as denuncias do usuario e manipular elas (editar, deletar, etc)

--- front/src/components/CardDenH.tsx
+++ front/src/components/CardDenH.tsx
@@ -1,10 +1,18 @@
 //chakra
-import {Card, CardBody, CardFooter, Button, Heading, Image, Stack, Text } from "@chakra-ui/react"; 
+import {Card, CardBody, CardFooter, Button, Heading, Image, Stack, Text, Modal, ModalOverlay,
+ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton } from "@chakra-ui/react"; 
 
 //imgs
 import aguaEstanc from "../img/aguaEstancada.png";
 
+//hooks (react, chakra, blalblabla)
+import { useDisclosure } from "@chakra-ui/react";
+
+
+
 const CardDenH = () => { //card de denúncoa horizontal, pra home do usuário
+
+    const {isOpen, onOpen, onClose} = useDisclosure();
 
     return(
         <Card border='1px solid #A9A9A9' w='650px'  borderRadius='4px' direction={{base: 'column', sm: 'row'}} _hover={{boxShadow: 'dark-lg', transition: '0.1s'}}>
@@ -19,14 +27,31 @@ const CardDenH = () => { //card de denúncoa horizontal, pra home do usuário
                 </CardBody>
 
                 <CardFooter>
-                    <Button variant='solid' color='white' bgColor="#338BB0" _hover={{backgroundColor: "gray.100", color: "#338BB0"}}>
-                        Ver denúncia
+                    <Button onClick={onOpen} variant='solid' color='white' bgColor="#338BB0" _hover={{backgroundColor: "gray.100", color: "#338BB0"}}>
+                        Gerenciar denúncia
                     </Button>
+                    <Modal isOpen={isOpen} onClose={onClose}>
+                        <ModalOverlay/>
+                        <ModalContent>
+                            <ModalHeader>Gerenciar denúncias</ModalHeader>
+                            <ModalCloseButton/>
+                            <ModalBody>
+                                <Text>Aqui será a seção de gerenciamento das denuncias do usuário</Text>
+                                <Text>Vão ter as opções de editar, deletar, etc</Text>
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button colorScheme="blue" onClick={onClose}>
+                                    Fechar
+                                </Button>
+                                <Button variant='ghost'>ação secundária</Button>
+                            </ModalFooter>
+                        </ModalContent>
+                    </Modal>
                 </CardFooter>
             </Stack>
         </Card>
     )
 }
-
+ // usar modal pra abrir a denuncia e deixar o usuario editar ela ou apagar, crud da denuncia
 
 export default CardDenH;
