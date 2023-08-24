ec130dd08cbc28ac1675ec85709a2457b97404c4
Kaua2123 <kauantnetsoares@gmail.com>
2023-08-24 18:47:31 -0300

    alterações componente de card tarefa - rayan

--- front/src/components/CardTarefa.tsx
+++ front/src/components/CardTarefa.tsx
@@ -1,15 +1,60 @@
-import { Box, IconButton } from "@chakra-ui/react"
-import { PiPlusBold } from "react-icons/pi";
+import { Box,  Modal,    ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure, Button, Text, Input, Textarea, Flex  } 
+from "@chakra-ui/react"
+import { TfiPlus } from "react-icons/tfi";
 
 
 
-const CardTarefa = () => {
-    return(
+const CardTarefa = () => {  
+    const  { isOpen, onOpen, onClose } = useDisclosure()
+    return(     
         
  
-     <Box bg='#338BB0' fontSize='20px' w='80%' p={3} color='white'>
+     <Box bg='#338BB0' fontSize='20px' borderRadius='10px' w='80%' p={3} color='white'>
         El agua estas estancada
-     <IconButton aria-label="kkkk" bgColor="#338BB0" color="white" _hover={{}} icon={<PiPlusBold/>}></IconButton> 
+     <Button onClick={onOpen} w='100px' h='30px' ml='130px' leftIcon={<TfiPlus/>} color='black'>Abrir</Button>
+     <Modal size='5xl' isOpen={isOpen} onClose={onClose}>
+    <ModalOverlay />
+     <ModalContent>
+        <ModalHeader></ModalHeader>
+        <ModalCloseButton/>
+        <ModalBody>
+
+        <Flex flexDirection="column">
+              <Box display="flex" justifyContent="space-between">
+                <Box>
+                  <Text fontSize="20px">
+                    <b>Adicionar subtarefa</b>
+                  </Text>
+                  <Input type="text" w="200px" borderColor="gray" />
+                  <Button bgColor="#338BB0" color="white" _hover={{ backgroundColor: "white", color: "#338BB0" }} mt="10px">
+                    Adicionar
+                  </Button>
+                </Box>
+                <Box>
+                  <Text fontSize="20px">
+                    <b>Enviar mensagem para o denunciante</b>
+                  </Text>
+                  <Textarea borderColor="gray" />
+                </Box>
+              </Box>
+            </Flex>
+          </ModalBody>
+      
+              
+                
+         
+     
+        
+     
+        <ModalFooter>
+        
+        <Button colorScheme='blue' mr={3} onClick={onClose}>
+            Fechar
+        </Button>
+        </ModalFooter>
+     </ModalContent>
+
+     </Modal>
     </Box>
 
     )
