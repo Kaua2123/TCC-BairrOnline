cb2048fd6d0bcac602ba28de9f8c22c711aa5293
Kaua2123 <kauantnetsoares@gmail.com>
2023-08-25 18:35:42 -0300

    alteraçoes no card de tarefa - rayan

--- front/src/components/CardTarefa.tsx
+++ front/src/components/CardTarefa.tsx
@@ -1,11 +1,24 @@
-import { Box,  Modal,    ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure, Button, Text, Input, Textarea, Flex  } 
+import { Box,  Modal,    ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure, 
+  Button, Text, Input, Textarea, Flex, Stack, Step, StepDescription, StepIcon, StepIndicator, StepNumber, StepSeparator,
+  StepStatus, StepTitle, Stepper, useSteps } 
 from "@chakra-ui/react"
 import { TfiPlus } from "react-icons/tfi";
+
+const steps = [
+  { title: '20%', description: "Saneamento Básico" },
+  { title: 'Second', description: 'Date & Time' },
+  { title: 'Third', description: 'Select Rooms' },
+]
 
 
 
 const CardTarefa = () => {  
-    const  { isOpen, onOpen, onClose } = useDisclosure()
+    const  { isOpen,  onOpen, onClose } = useDisclosure()
+    const { activeStep } = useSteps({
+      index: 1,
+      count: steps.length,
+    })
+  
     return(     
         
  
@@ -18,15 +31,36 @@ const CardTarefa = () => {
         <ModalHeader></ModalHeader>
         <ModalCloseButton/>
         <ModalBody>
-
-        <Flex flexDirection="column">
-              <Box display="flex" justifyContent="space-between">
+        <Stepper size='lg' index={activeStep}>
+        {steps.map((step, index) => (
+           <Step key={index}>
+             <StepIndicator>
+              <StepStatus
+                complete={<StepIcon />}
+                incomplete={<StepNumber />}
+                active={<StepNumber />}
+               />
+             </StepIndicator>
+             <Box flexShrink='0'>
+             <StepTitle>{step.title}</StepTitle>
+              <StepDescription>{step.description}</StepDescription>
+          </Box>
+          <StepSeparator />
+        </Step>
+        ))}
+       </Stepper> 
+    
+            <Flex flexDirection="column">
+          
+      
+              <Box mt='200px' display="flex" justifyContent="space-between">
                 <Box>
                   <Text fontSize="20px">
                     <b>Adicionar subtarefa</b>
                   </Text>
-                  <Input type="text" w="200px" borderColor="gray" />
-                  <Button bgColor="#338BB0" color="white" _hover={{ backgroundColor: "white", color: "#338BB0" }} mt="10px">
+                  <Input type="text" w='150px' borderColor="gray" mt='10px' />
+                  <Stack></Stack>
+                  <Button bgColor="#338BB0" color="white" _hover={{ backgroundColor: "white", color: "#338BB0" }} mt='-67px' ml='160px'>
                     Adicionar
                   </Button>
                 </Box>
@@ -39,18 +73,7 @@ const CardTarefa = () => {
               </Box>
             </Flex>
           </ModalBody>
-      
-              
-                
-         
-     
-        
-     
         <ModalFooter>
-        
-        <Button colorScheme='blue' mr={3} onClick={onClose}>
-            Fechar
-        </Button>
         </ModalFooter>
      </ModalContent>
 
@@ -58,7 +81,7 @@ const CardTarefa = () => {
     </Box>
 
     )
-}
+    }
 
 
 
