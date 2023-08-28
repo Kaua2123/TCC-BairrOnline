a0c140d9925669699fa1b94faaba9b54d86c646c
Kaua2123 <kauantnetsoares@gmail.com>
2023-08-28 19:05:36 -0300

    back end do acompanhamento e mudanças card tarefa - rayan

--- front/src/components/CardTarefa.tsx
+++ front/src/components/CardTarefa.tsx
@@ -1,88 +1,91 @@
-import { Box,  Modal,    ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure, 
-  Button, Text, Input, Textarea, Flex, Stack, Step, StepDescription, StepIcon, StepIndicator, StepNumber, StepSeparator,
-  StepStatus, StepTitle, Stepper, useSteps } 
+import { Box,  Modal,ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure, 
+  Button, Text, Input, Textarea, Flex, Stack, Step ,StepDescription, StepIcon, StepIndicator, StepSeparator,
+  StepStatus, StepTitle, Stepper, useSteps,  } 
 from "@chakra-ui/react"
 import { TfiPlus } from "react-icons/tfi";
 
-const steps = [
-  { title: '20%', description: "Saneamento Básico" },
-  { title: 'Second', description: 'Date & Time' },
-  { title: 'Third', description: 'Select Rooms' },
-]
 
+const steps = [
+  { title: '20%', description: 'Saneamento básico.' },
+  { title: '50%', description: 'kkkkk' },
+  { title: '100%', description: 'Solucionado' },
+]
 
 
 const CardTarefa = () => {  
     const  { isOpen,  onOpen, onClose } = useDisclosure()
-    const { activeStep } = useSteps({
+    const { activeStep, setActiveStep } = useSteps({
       index: 1,
       count: steps.length,
     })
-  
-    return(     
-        
- 
+    return(      
      <Box bg='#338BB0' fontSize='20px' borderRadius='10px' w='80%' p={3} color='white'>
-        El agua estas estancada
-     <Button onClick={onOpen} w='100px' h='30px' ml='130px' leftIcon={<TfiPlus/>} color='black'>Abrir</Button>
+    El agua estas estancada
+     <Button onClick={onOpen} w='100px' h='30px' ml='130px' leftIcon={<TfiPlus/>} _hover={{backgroundColor: 'acqua', color: '#338bb0'}} 
+     color='black'>Abrir</Button>
      <Modal size='5xl' isOpen={isOpen} onClose={onClose}>
     <ModalOverlay />
      <ModalContent>
         <ModalHeader></ModalHeader>
         <ModalCloseButton/>
         <ModalBody>
-        <Stepper size='lg' index={activeStep}>
-        {steps.map((step, index) => (
-           <Step key={index}>
-             <StepIndicator>
-              <StepStatus
-                complete={<StepIcon />}
-                incomplete={<StepNumber />}
-                active={<StepNumber />}
-               />
-             </StepIndicator>
-             <Box flexShrink='0'>
-             <StepTitle>{step.title}</StepTitle>
-              <StepDescription>{step.description}</StepDescription>
+          <Flex flexDirection="column">
+          <Stepper size='lg' index={activeStep}>
+      {steps.map((step, index) => (
+        <Step key={index} onClick={() => setActiveStep(index)}>
+          <StepIndicator>
+            <StepStatus
+              complete={<StepIcon />}
+           
+            />
+          </StepIndicator>
+
+          <Box flexShrink='0'>
+            <StepTitle>{step.title}</StepTitle>
+            <StepDescription>{step.description}</StepDescription>
           </Box>
+
           <StepSeparator />
         </Step>
-        ))}
-       </Stepper> 
-    
-            <Flex flexDirection="column">
-          
-      
-              <Box mt='200px' display="flex" justifyContent="space-between">
-                <Box>
+      ))}
+    </Stepper>
+            <Flex justifyContent="space-between" mt="20px">    
+            <Flex flexDirection="row">
+              <Box>
+                <Box mt="100px">
                   <Text fontSize="20px">
                     <b>Adicionar subtarefa</b>
                   </Text>
                   <Input type="text" w='150px' borderColor="gray" mt='10px' />
                   <Stack></Stack>
-                  <Button bgColor="#338BB0" color="white" _hover={{ backgroundColor: "white", color: "#338BB0" }} mt='-67px' ml='160px'>
+                  <Button bgColor="#338BB0" color="white" _hover={{ backgroundColor: "white", color: "#338BB0" }} 
+                  mt='-67px' ml='160px'>
                     Adicionar
                   </Button>
                 </Box>
-                <Box>
+                <Box ml="600px" mt="-110px">
                   <Text fontSize="20px">
                     <b>Enviar mensagem para o denunciante</b>
                   </Text>
                   <Textarea borderColor="gray" />
+                  <Button bgColor="#338BB0" color="white" _hover={{ backgroundColor: "white", color: "#338BB0" }} mt='10px'
+                   ml='300px'>
+                    Enviar
+                  </Button>
                 </Box>
-              </Box>
-            </Flex>
+              </Box>  
+              </Flex>
+              </Flex>
+              </Flex>
           </ModalBody>
-        <ModalFooter>
-        </ModalFooter>
+           <ModalFooter>
+           </ModalFooter>
      </ModalContent>
-
+         
      </Modal>
     </Box>
-
     )
     }
-
 
 
 
---
+++ server/src/controllers/acompanhamento/index.js
@@ -0,0 +1,43 @@
+const knex = require('../../database')
+ 
+module.exports = {
+    async criarAcompanhamento(req, res) {
+        try {
+            const { denuncias_den_cod } = req.body;
+            const { aco_progresso } = req.body;
+            const { instituicao_usuario_usu_cod } = req.body;
+
+              
+            const denunciaExists = await knex('denuncias').where('denuncias_den_cod').first();
+             if (!denunciaExists) {
+                return res.status(404).json({ error:'Denúncia não encontrada'});                            
+         }      
+       
+         const aco_data = new Date().toISOString();
+         await knex('acompanhamento').insert({
+           denuncias_den_cod: denunciaCod,
+           aco_data, 
+           aco_progresso: progresso,
+
+         });
+            return res.status(201).json({ message: 'Acompanhamento criado com sucesso.'});
+
+        } catch(error){
+              return res.status(400).json({ error: error.message });
+        }
+    }, 
+
+    async getAcompanhamentos(req, res) {
+        try {
+            const { denunciaCod } = req.params;
+            
+        
+             const denunciaExists = await knex('denuncias').where('den_cod',  denunciaCod).first();
+             if(!denunciaExits) {
+                return res.status(404).json({ error: 'Denúncia nao encontrada'})
+             }
+        } catch (error) {
+            return res.status(400).json({error: error.message});
+        }
+    }
+}
