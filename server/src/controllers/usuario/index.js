398038f204678914a7512ef7c9a05a3600fcd9f3
Kaua2123 <kauantnetsoares@gmail.com>
2023-08-30 20:08:45 -0300

    login funfando e gerando token de autenticaçao

--- server/src/controllers/usuario/index.js
+++ server/src/controllers/usuario/index.js
@@ -1,8 +1,7 @@
+require('dotenv').config({path: '../.env'}); // lendo o arquivo env
 const knex = require('../../database/banco');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const chaveJWT = 'jwtCHAVE';
-
 
 module.exports = {
     async raiz(req, res){
@@ -54,6 +53,7 @@ module.exports = {
     async logarUsuario(req, res){ //login, autentica usuários
         try {
 
+
             const { usu_email } = req.body;
             const { usu_senha } = req.body;
 
@@ -61,25 +61,25 @@ module.exports = {
 
             if (usuario.length > 0){
 
-                const senha = usu_senha;
-                const senhaCorrespondente = await bcrypt.compare(usu_senha, senha);
+                const senhaCorrespondente = await bcrypt.compare(usu_senha, usuario[0].usu_senha); 
+                console.log('senha correspondente:', senhaCorrespondente);
 
-              
+                if (senhaCorrespondente) {
                     //senha válida, logo irá gerar um token JWT.
-
-                    const token = jwt.sign({usu_cod: usuario[0].usu_cod}, chaveJWT, {expiresIn: '1h'});
+                    console.log('Credenciais corretas');
+                    const token = jwt.sign({usu_cod: usuario[0].usu_cod}, process.env.CHAVE_JWT, {expiresIn: '1h'});
                     console.log(token);
                     return res.status(200).json({message: 'Login sucedido', auth: true, token: token});
 
-        
-             
+                } else {
+                    return res.status(401).json({ message: 'E-mail ou senha incorretos.' });
+                }
 
             } else {
                 return res.status(401).json({ message: 'Não há usuários cadastrados.' });
             }
           
         } catch (error) {
-            console.error(error);
             return res.status(500).json({ error: error.message });
         }
     }
