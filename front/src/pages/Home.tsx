b1c1076f912934d650f1fe3ec14f77fed3959e34
Kaua2123 <kauantnetsoares@gmail.com>
2023-10-09 21:30:28 -0300

    layout

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -13,12 +13,14 @@ import { useEffect } from 'react';
 import jwt_decode from 'jwt-decode';
 
 //chakra
-import { ChakraProvider, Center, Box, Flex, Image, Text, Container, Card, CardHeader, CardBody, CardFooter
-, Heading, Modal, ModalOverlay, ModalBody, ModalContent, ModalHeader, ModalFooter, ModalCloseButton, Wrap, WrapItem, Button, Divider, Input, InputGroup, InputLeftElement, extendTheme, Grid, GridItem, useDisclosure, useColorMode, theme, Avatar, Tag, TagLeftIcon, HStack, Stack, VStack} from '@chakra-ui/react';
+import {
+  ChakraProvider, Center, Box, Flex, Image, Text, Container, Card, CardHeader, CardBody, CardFooter
+  , Heading, Modal, ModalOverlay, ModalBody, ModalContent, ModalHeader, ModalFooter, ModalCloseButton, Wrap, WrapItem, Button, Divider, Input, InputGroup, InputLeftElement, extendTheme, Grid, GridItem, useDisclosure, useColorMode, theme, Avatar, Tag, TagLeftIcon, HStack, Stack, VStack
+} from '@chakra-ui/react';
 import { modalAnatomy as parts } from '@chakra-ui/anatomy'
 import { createMultiStyleConfigHelpers, defineStyle } from '@chakra-ui/styled-system'
 //componentes
-import  Header, { HeaderADM, HeaderInst, HeaderUsu }  from '../components/Header';
+import Header, { HeaderADM, HeaderInst, HeaderUsu } from '../components/Header';
 import Footer from '../components/Footer';
 import theme from '../breakpoints.tsx';
 
@@ -54,11 +56,14 @@ const Home = () => {
 
   const [denuncias, setDenuncias] = useState([]);
   const [instituicoes, setInstituicoes] = useState([]);
+  const [temInst, setTemInst] = useState(false);
   const [temDenuncia, setTemDenuncia] = useState(false);
+  const [pesquisa, setPesquisa] = useState("");
+  const [instFiltradas, setInstFiltradas] = useState([]);
 
   const [comments, setComments] = useState([]);
 
-  const {colorMode} = useColorMode();
+  const { colorMode } = useColorMode();
 
   const token = localStorage.getItem('token');
   const decodificaToken = token ? jwt_decode(token) : null;
@@ -82,7 +87,7 @@ const Home = () => {
 
   if (isTokenExpired()) {
     localStorage.removeItem('token');
-    
+
   }
 
   let headerComponent = null;
@@ -95,32 +100,34 @@ const Home = () => {
     headerComponent = <HeaderADM />
   }
   else {
-    headerComponent = <Header/>;
+    headerComponent = <Header />;
   }
 
- 
 
-  const handleCommentSubmit = (comment) => {setComments(
-    [
-      ...comments, comment
-    ]
-  )}
+
+  const handleCommentSubmit = (comment) => {
+    setComments(
+      [
+        ...comments, comment
+      ]
+    )
+  }
 
   const [rep, setrep] = useState(false);
 
   async function getDenuncia() {
     axios.get('http://localhost:3344/getDenuncia')
-    .then(response => {
-      setDenuncias(response.data);
+      .then(response => {
+        setDenuncias(response.data);
 
-      if(response.data.length > 0){
-        setTemDenuncia(true);
-      }
+        if (response.data.length > 0) {
+          setTemDenuncia(true);
+        }
 
-    })
-    .catch(error => {
-      console.error(error);
-    })
+      })
+      .catch(error => {
+        console.error(error);
+      })
   }
 
   useEffect(() => {
@@ -129,38 +136,50 @@ const Home = () => {
 
   async function getInstituicoes() {
     axios.get('http://localhost:3344/getInstituicoes')
-    .then(response => {
-      setInstituicoes(response.data);
+      .then(response => {
+        setInstituicoes(response.data);
 
-    })
-    .catch(error => {
-      console.error(error);
-    })
+        if (response.data.length > 0) {
+          setTemInst(true);
+        }
+
+      })
+      .catch(error => {
+        console.error(error);
+      })
   }
 
   useEffect(() => {
     getInstituicoes();
   }, [])
 
+  const aoPesquisar = (e) => {
+    if (e.key === 'Enter') {
 
- return (
-
-  // BAIRRONLINE GRANDAO TLG (usando os componentes de estilo do chakra)
-
-  <ChakraProvider theme={theme} >
-
-  {headerComponent}
+      const instFiltradas = instituicoes.filter((instituicao) => instituicao.usu_nome.toLowerCase().includes(pesquisa.toLowerCase()));
+      setInstFiltradas(instFiltradas);
+    }
+  }
 
 
-    <Grid templateColumns={{base: '1fr', sm: '1fr 2fr 1fr'}} gap={4} bgColor='#338bb0'>
+  return (
+
+    // BAIRRONLINE GRANDAO TLG (usando os componentes de estilo do chakra)
+
+    <ChakraProvider theme={theme} >
+
+      {headerComponent}
+
+
+      <Grid templateColumns={{ base: '1fr', sm: '1fr 2fr 1fr' }} gap={4} bgColor='#338bb0'>
         {/* container da esquerda */}
         <GridItem colSpan={{ base: 1, md: 1, lg: 1 }}>
 
-          <Container centerContent mt={{base: '55px', md: '90px', lg:'120px'}}>
-            <Text color="white" textDecoration="underline" fontFamily="BreeSerif-Regular" whiteSpace="nowrap"fontSize={{ base: "15px", md: "24px", lg: "35px" }}>
+          <Container centerContent mt={{ base: '55px', md: '90px', lg: '120px' }}>
+            <Text color="white" textDecoration="underline" fontFamily="BreeSerif-Regular" whiteSpace="nowrap" fontSize={{ base: "15px", md: "24px", lg: "35px" }}>
               Com problemas no bairro?
             </Text>
-            <Text textAlign="center"color="white" fontFamily="BreeSerif-Regular" fontSize={{ base: "13px", md: "15px", lg: "18px" }} fontWeight="normal">
+            <Text textAlign="center" color="white" fontFamily="BreeSerif-Regular" fontSize={{ base: "13px", md: "15px", lg: "18px" }} fontWeight="normal">
               Relate suas denúncias aqui, e tenha o retorno das instituições responsáveis pela resolução dos problemas
             </Text>
           </Container>
@@ -180,7 +199,7 @@ const Home = () => {
 
         {/* container com os textos na direita */}
         <GridItem colSpan={{ base: 1, md: 1, lg: 1 }}>
-          <Container centerContent mt={{base: '55px', md: '90px', lg:'120px'}}>
+          <Container centerContent mt={{ base: '55px', md: '90px', lg: '120px' }}>
             <Text
               color="white"
               textDecoration="underline"
@@ -202,113 +221,131 @@ const Home = () => {
           </Container>
         </GridItem>
       </Grid>
-         {/*------------FIM FULLSCREEN------------*/}
+      {/*------------FIM FULLSCREEN------------*/}
 
-           {/* ------------SEÇÃO DENUNCIAS E INSTITUIÇOES AQUI------------*/}
-         <Box  w="100%" bgColor='gray.200' h='full'  borderRadius='4px'>
+      {/* ------------SEÇÃO DENUNCIAS E INSTITUIÇOES AQUI------------*/}
+      <Box w="100%" bgColor='gray.200' h='full' borderRadius='4px'>
 
-             <HStack  w='full' h='60vh'>
-                 <Flex w='full' h='full' display={{base: 'none', md: 'flex'}} alignItems='center' justifyContent='center' >
+        {temDenuncia ? (
+          <HStack w='full' h='60vh'>
+            <Flex w='full' h='full' display={{ base: 'none', md: 'flex' }} alignItems='center' justifyContent='center' >
 
               <VStack>
-                 <Heading color='#338bb0' fontSize='3xl'  fontFamily='BreeSerif-Regular' fontWeight='normal'>Denúncias em alta </Heading>
-                  <Text fontSize='19px' mr={6}>Denúncias em alta dos <br /> bairros de  <b>Volta Redonda</b></Text>
-          
+                <Heading color='#338bb0' fontSize='3xl' fontFamily='BreeSerif-Regular' fontWeight='normal'>Denúncias em alta </Heading>
+                <Text fontSize='19px' mr={6}>Denúncias em alta dos <br /> bairros de  <b>Volta Redonda</b></Text>
+
               </VStack>
-                 </Flex>
+            </Flex>
 
 
-                 <Flex w='full' h='full' mr={5} alignItems='center' justifyContent='center'>
-         
-
-{/*       
-     {denuncias.map((denuncia, index) => (
-        <CardGrande denuncia={denuncia}/>
-     ))}
-        */}
-
-        {temDenuncia && <CardGrande denuncia={denuncias[0]} />}
- 
-
-                 </Flex>
-                </HStack>
+            <Flex w='full' h='full' mr={5} alignItems='center' justifyContent='center'>
 
 
-                  {temDenuncia ? (
-                     <SlideDen denuncias={denuncias}/>
-                  ) : (
-                    <>
-                    <Box bgColor='white' boxShadow='lg'>
-                    <Flex justify='center'>
-                      <Image src={denunciaNotFound}></Image> {/*img temporaria */}
-                    </Flex>
-                    <Flex justify='center'>
-                      <Text fontSize='35px' fontFamily='BreeSerif-Regular' fontWeight='normal' p={8}  color='#338bb0' mt='-50px'>Parece que não há nenhuma denúncia em alta...</Text>
-                    </Flex>
-                    </Box>
-                    </>
-                  )}
+              {/*       
+{denuncias.map((denuncia, index) => (
+<CardGrande denuncia={denuncia}/>
+))}
+*/}
 
-   {/* ------------AQUI ACABAM DENUNCIAS------------*/}
+              <CardGrande denuncia={denuncias[0]} />
 
-           <HStack w='full' h='100vh'>
 
-              <Flex w='full' h='full' flexDirection='column' alignItems='center' >
+            </Flex>
+          </HStack>
+        ) : (
+          <VStack w='full' h='60vh'>
+            <Flex w='full' h='full' display={{ base: 'none', md: 'flex' }} alignItems='center' justifyContent='center'>
+              <Box borderRadius='12px' bgColor='white' p={10} boxShadow='lg'>
+                <Text color='#338bb0' fontSize='40px' fontFamily='BreeSerif-Regular'>Nenhuma denúncia em alta encontrada...</Text>
+                <Text fontSize='20px'>Continue a denunciar!</Text>
+              </Box>
+            </Flex>
+          </VStack>
+        )}
 
-                <Flex gap={4} display={{base: 'flex', md: 'none'}} w='full' maxW='md' p={6}>
-                    <Heading fontSize='2xl' color='#338bb0' >Instituições</Heading>
-                    <InputGroup >  
-                            <InputLeftElement pointerEvents='none'>
-                                <FiSearch color='white' />
-                            </InputLeftElement>
-                            <Input  color='white' placeholder='Busque por instituições' _placeholder={{color: "white"}} bg='#338BB0'/>
-                    </InputGroup>
-                </Flex>
-              
+
+
+        {temDenuncia ? (
+          <SlideDen denuncias={denuncias} />
+        ) : (
+          <>
+            <Box bgColor='white' boxShadow='lg'>
+              <Flex justify='center'>
+                <Image src={denunciaNotFound}></Image> {/*img temporaria */}
+              </Flex>
+              <Flex justify='center'>
+                <Text fontSize='35px' fontFamily='BreeSerif-Regular' fontWeight='normal' p={8} color='#338bb0' mt='-50px'>Parece que não há nenhuma denúncia em alta...</Text>
+              </Flex>
+            </Box>
+          </>
+        )}
+
+        {/* ------------AQUI ACABAM DENUNCIAS------------*/}
+
+        {temInst ? (
+          <HStack w='full' h='100vh'>
+
+            <Flex w='full' h='full' flexDirection='column' alignItems='center' >
+
+              <Flex gap={4} display={{ base: 'flex', md: 'none' }} w='full' maxW='md' p={6}>
+                <Heading fontSize='2xl' color='#338bb0' >Instituições</Heading>
+                <InputGroup >
+                  <InputLeftElement pointerEvents='none'>
+                    <FiSearch color='white' />
+                  </InputLeftElement>
+                  <Input value={pesquisa} onChange={(e) => setPesquisa(e.target.value)} onKeyPress={aoPesquisar}  color='white' placeholder='Busque por instituições' _placeholder={{ color: "white" }} bg='#338BB0' />
+                </InputGroup>
+              </Flex>
+
               <Flex>
 
-                    <Grid templateColumns={{base: '1fr', md: '1fr 1fr 1fr'}} gap={{base: 16, md: 4}} p={20}>
-  {instituicoes.map((instituicao) => (
-    <GridItem key={instituicao.usu_cod}>
-      <CardInst instituicao={instituicao} />
-    </GridItem>
-  ))}
-</Grid>
-</Flex>      
-
-                 
+                <Grid templateColumns={{ base: '1fr', md: '1fr 1fr 1fr' }} gap={{ base: 16, md: 4 }} p={20}>
+                  {instituicoes.map((instituicao) => (
+                    <GridItem key={instituicao.usu_cod}>
+                      <CardInst instituicao={instituicao} />
+                    </GridItem>
+                  ))}
+                </Grid>
               </Flex>
 
-              <Flex w='full' h='full' alignItems='center' display={{base: 'none', md: 'flex'}} justifyContent='center' >
-                  <Stack w='full' maxW='md' spacing={4}>
-                  <Heading fontSize={{base: '1xl', md: '3xl'}} color='#338bb0' fontFamily='BreeSerif-Regular' fontWeight='normal'>Instituições</Heading>
-                 
-                    <Text fontSize={{ base: '16px', md: '19px'}}>
-                      Conheça as <b>instituições</b> que poderão resolver os problemas de seu <b>bairro</b>
-                    </Text>
-                    
-                    <InputGroup >  
-                            <InputLeftElement pointerEvents='none'>
-                                <FiSearch color='white' />
-                            </InputLeftElement>
-                            <Input /* onSubmit= */ w='280px' color='white' placeholder='Busque por instituições' _placeholder={{color: "white"}} bg='#338BB0'/>
-                        </InputGroup>
-              
-                  </Stack>
-          
 
-         
+            </Flex>
 
-              </Flex>
-        
+            <Flex w='full' h='full' alignItems='center' display={{ base: 'none', md: 'flex' }} justifyContent='center' >
+              <Stack w='full' maxW='md' spacing={4}>
+                <Heading fontSize={{ base: '1xl', md: '3xl' }} color='#338bb0' fontFamily='BreeSerif-Regular' fontWeight='normal'>Instituições</Heading>
+
+                <Text fontSize={{ base: '16px', md: '19px' }}>
+                  Conheça as <b>instituições</b> que poderão resolver os problemas de seu <b>bairro</b>
+                </Text>
+
+
+              </Stack>
+
+
+
+
+            </Flex>
+
           </HStack>
-           
-         </Box>
+
+        ) : (
+          <VStack w='full' h='60vh'>
+            <Flex w='full' h='full' display={{ base: 'none', md: 'flex' }} alignItems='center' justifyContent='center'>
+              <Box borderRadius='12px' bgColor='white' p={10} boxShadow='lg'>
+                <Text color='#338bb0' fontSize='40px' fontFamily='BreeSerif-Regular'>Nenhuma instituição cadastrada...</Text>
+                <Text fontSize='20px'>Continue a denunciar!</Text>
+              </Box>
+            </Flex>
+          </VStack>
+          )}
+
+      </Box>
       {/* ------------E AQUI INSTITUIÇOES------------*/}
 
 
-    <Footer/>
-     </ChakraProvider>
+      <Footer />
+    </ChakraProvider>
 
   );
 }
