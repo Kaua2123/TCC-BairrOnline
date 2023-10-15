dc1d74928e499cf7cd152875f266009be7dba602
Kaua2123 <kauantnetsoares@gmail.com>
2023-10-15 20:47:07 -0300

    menu de navegação

--- mobile/App.js
+++ mobile/App.js
@@ -3,15 +3,28 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './screens/Home'; // Importe a tela "Home" aqui
 import HomeUsu from './screens/HomeUsu';
+import Denuncie from './screens/Denuncie';
+import VerDen from './screens/VerDen';
+import Icon from 'react-native-vector-icons/Feather';
 
 const Tab = createBottomTabNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} /> 
-        <Tab.Screen name="HomeUsu" component={HomeUsu}></Tab.Screen>
+      <Tab.Navigator screenOptions={{
+        tabBarStyle: {backgroundColor: 'black', border: 'none'},
+        tabBarActiveBackgroundColor: 'white',
+        tabBarActiveTintColor: '#338bb0',
+        tabBarInactiveTintColor: 'white',
+      }}>
+        <Tab.Screen name="Home" component={Home} options={{tabBarIcon: ({focused, size}) => (
+          
+          <Icon name='home' size={25} color={focused ? '#338bb0' : 'white'}/>
+        )}} /> 
+        {/* <Tab.Screen name="HomeUsu" component={HomeUsu}></Tab.Screen> */}
+        <Tab.Screen name="Denuncie aqui" component={Denuncie}></Tab.Screen>
+        <Tab.Screen name="Ver Denúncias" component={VerDen}></Tab.Screen>
       </Tab.Navigator>
     </NavigationContainer>
   );
--- mobile/screens/VerDen.js
+++ mobile/screens/VerDen.js
@@ -3,14 +3,18 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 
 
-function Home() {
+function VerDen() {
     
     const navigation = useNavigation();
   return (
     <View>
-      <Text>home aqui</Text>
+      <Text>denuncie aqui</Text>
+
+      <Button title='ir pra home' onPress={() => {
+        navigation.navigate('Home');
+      }}></Button>
     </View>
   );
 }
 
-export default Home;
+export default VerDen;
